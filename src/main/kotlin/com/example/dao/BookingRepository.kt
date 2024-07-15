28f18220c8ad357e631baf1c0157fda340433c69
vickram <vickramodero6@gmail.com>
2024-07-15 09:02:20 +0300

    added build to be pushed

--- src/main/kotlin/com/example/dao/BookingRepository.kt
+++ src/main/kotlin/com/example/dao/BookingRepository.kt
@@ -4,13 +4,28 @@ import com.example.db.booking.Booking
 import com.example.models.booking.BookingRequest
 import com.example.models.booking.BookingResponse
 import com.example.models.booking.UpdateBookingStatus
-import java.util.UUID
+import java.util.*
 
 interface BookingRepository {
-    suspend fun createNewBooking(customerId: UUID, bookingRequest: BookingRequest): BookingResponse
-    suspend fun getFilteredBookings(filter: (Booking) -> Boolean): List<BookingResponse>
-    suspend fun updateBookingStatus(id: String, bookingStatusUpdate: UpdateBookingStatus): Boolean
-    suspend fun updateBooking(customerId: UUID, id: String, bookingUpdateRequest: BookingRequest): Boolean
+    suspend fun createNewBooking(
+        customerId: UUID,
+        bookingRequest: BookingRequest
+    ): BookingResponse
+
+    suspend fun getFilteredBookings(
+        filter: (Booking) -> Boolean
+    ): List<BookingResponse>
+
+    suspend fun updateBookingStatus(
+        id: String,
+        bookingStatusUpdate: UpdateBookingStatus
+    ): Boolean
+
+    suspend fun updateBooking(
+        customerId: UUID,
+        id: String,
+        bookingUpdateRequest: BookingRequest
+    ): Boolean
     suspend fun deleteBooking(id: String): Boolean
     suspend fun clearBookings(): Boolean
 }
