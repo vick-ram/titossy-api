b353c322271dedd721379b5ea1ff0ea0d402616b
vickram <vickramodero6@gmail.com>
2024-07-17 01:03:58 +0300

    ever problem

--- build.gradle.kts
+++ build.gradle.kts
@@ -65,6 +65,6 @@ dependencies {
     implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
     implementation("io.ktor:ktor-client-content-negotiation:$ktor_version")
-    implementation("com.pusher:push-notifications-server-java:1.1.1")
+    //implementation("com.pusher:push-notifications-server-java:1.1.1")
 
 }
